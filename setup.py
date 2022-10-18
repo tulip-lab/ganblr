a7665d0c5f30c2ac40b03306a383e9182acaf393
kae zhou <kaemihara@outlook.com>
2022-10-18 15:28:47 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+path = os.path.abspath(os.path.dirname(__file__))
+
+try:
+  with open(os.path.join(path, 'README.md')) as f:
+    long_description = f.read()
+except Exception as e:
+  long_description = "Ganblr Toolbox"
+
+setup(
+    name = "ganblr",
+    version = "0.1.0",
+    keywords = ("tensorflow2.x", "ganblr", "tulip"),
+    description = "Ganblr Toolbox",
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    python_requires=">=3.5.0",
+    license = "MIT Licence",
+
+    url = "https://github.com/tulip-lab/ganblr-toolbox",
+    author = "kae zhou",
+    author_email = "kaezhou@gmail.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = ["numpy", "tensorflow", "pyitlab", "pgmpy"],
+    platforms = "any",
+
+    scripts = [],
+    entry_points = {}
+)
